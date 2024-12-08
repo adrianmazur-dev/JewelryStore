3ceb36b6febd968e6270075d64a04a6d2b77c62d
Adrian Mazur <adrianmazur@zohomail.eu>
2024-12-08 18:28:30 +0100

    ustalenie struktury projektu, utworzenie bazy danych i początkowych migracji, dodanie readme.md i gitignore

---
+++ JewelryStore.Infrastructure/Data/JewelryStoreDbContext.cs
@@ -0,0 +1,30 @@
+﻿using JewelryStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelryStore.Infrastructure.Data
+{
+    public class JewelryStoreDbContext : DbContext
+    {
+        public JewelryStoreDbContext(DbContextOptions<JewelryStoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.Price).HasColumnType("decimal(18,2)");
+            });
+        }
+    }
+}
