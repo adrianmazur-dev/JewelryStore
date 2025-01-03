3730a1fe0f4f9496cdcf7f067068ed7350d15bea
Krupa144 <kacpi8450@gmail.com>
2025-01-03 12:34:41 +0100

    adding form, fixing layout

---
+++ JewelryStore.Web/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JewelryStore.Web.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitForm(string Name, string Email, string Message)
+        {
+            ViewBag.Message = "Dziękujemy za przesłanie formularza!";
+            return View("Index");
+        }
+    }
+
+}
